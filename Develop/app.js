a0c7fa7efb25c55329c27f8f9b59545931f2fdb4
sathya <karpagasathya@gmail.com>
2020-06-08 23:54:57 -0500

    file directory exist

--- Develop/app.js
+++ Develop/app.js
@@ -230,6 +230,9 @@ const generateTeam = () => {
   };
 
   const renderHtml = () => {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
     fs.writeFileSync(outputPath, render(teamMembers), "utf-8", (err) => {
       if (err) throw err;  
     });
